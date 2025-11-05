d737cabb3cb8fe6d88a0658770d26f13c504fe42
doker123 <dodo2larin@gmail.com>
2025-11-05 14:25:07 +0700

    Сохранил пароль от пользователей в txt

--- DesignPro/settings.py
+++ DesignPro/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)-!yqmlc#k26d$*vqv*@(==1e&x&8f2^0803786gyx0mk2^^1#'
+SECRET_KEY = 'django-insecure-)-!#k26d$*vqv*@(==1e&x&8f2^0803786gyx0mk2^^1#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
